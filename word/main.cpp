1ec5d1cb83b99a5dcd7c943cf9c0349b944bd0ec
Charles Ludden <charles.ludden@gmail.com>
2021-01-20 18:31:09 +0000

    completed main file

--- word/main.cpp
+++ word/main.cpp
@@ -1,91 +1,59 @@
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <vector>
-#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-void split(string const &text, char const &delim, vector<string> &vect){
-	vect = {};
-	string cWord = "";
-	char empty = ""[0];
-	char letter = ""[0];
-	if(delim == empty){
-		for(int i = 0; i < text.length(); i++){
-			letter = text[i];
-			cWord += letter;
-			vect.push_back(cWord);
-			cWord = "";
-		}
-	} else {
-		for(int i = 0; i < text.length(); i++){
-			letter = text[i];
-			if(letter != delim){
-				cWord += letter;
-				
-			} else {
-				vect.push_back(cWord);
-				cWord = "";
-			}
-		}
-		vect.push_back(cWord);
-	}
+bool isNotSpace(unsigned char ch) {
+    return !isspace(ch);
 }
 
-void dumpfile(string const &file, string &out){
-	ifstream ifs(file, ios::in);
-	string text = "";
-	string ct = "";
-	while(!ifs.eof()){
-		getline(ifs, ct);
-		text += ct + '\n';
-	}
-	ifs.close();
-	out = text;
+void trim(string &s) {
+    s.erase(find_if(s.rbegin(), s.rend(), &isNotSpace).base(), s.end());
 }
 
-void output(int &tChars, string &cLine){
-	ofstream ofile("word.out", ofstream::app);
-	string line = "";
-	vector<string> words;
-	split(cLine, ' ', words);
-				for(int i = 0; i < words.size(); i++){
-					line += words[i];
-					if(i != words.size()-1){
-						line += " ";
-					}
-				}
-	ofile << line << endl;
-	tChars = 0;
-	cLine = "";
-	ofile.close();
+void saveAndWrapLines(const char *ifs, const char *ofs) {
+    ifstream input(ifs, ios::in);
+    ofstream output(ofs, ofstream::trunc);
+
+    int numLines, colsPerLine;
+    string line;
+    string word;
+    
+    // first line has formatting settings
+    input >> numLines;
+    input >> colsPerLine;
+    
+    // clear the empty spaces in preparation to read the file
+    getline(input, line);
+    line.clear();
+
+    // trying to make the variables more descriptive
+    long totalChars = 0;
+    for(int i = 0; i < numLines && getline(input, word, ' '); i++) {
+        trim(word);
+        if(totalChars < colsPerLine && totalChars + word.length() <= colsPerLine){
+            totalChars += word.length();
+        } else {
+            output << line << endl;
+            line.clear();
+            totalChars = word.length();
+        }
+        
+        if (!word.empty()) {
+            line.append(word).append(" ");
+        }
+        
+        word.clear();
+    }
+    if (!line.empty()) {
+        output << line << endl;
+    }
 }
 
 int main(){
-	string text = "";
-	dumpfile("word.in", text);
-	vector<string> vect = {};
-	split(text, '\n', vect);
-	const string l1 = vect[0];
-	const string l2 = vect[1];
-	split(l1, ' ', vect);
-	const int N = stoi(vect[0]);
-	const int K  = stoi(vect[1]);
-	split(l2, ' ', vect);
-	string cLine = "";
-	int tChars = 0;
-	for(int i = 0; i < N; i++){
-		if(tChars < K && tChars + vect[i].length() <= K){
-			cLine += vect[i] + " ";
-			tChars += vect[i].length();
-			if(tChars == K || (tChars + vect[i+1].length()) > K){
-				output(tChars, cLine);
-			}
-			if(i == N-1){
-				output(tChars, cLine);
-			}
-		} else {
-			output(tChars, cLine);
-		}
-	}
+    saveAndWrapLines("word.in", "word.out");
+    return 0;
 }
